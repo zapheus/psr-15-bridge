4d452a09090e77eb3e294f03302a91bd9d9e4207
Rougin Gutib <rougingutib@gmail.com>
2020-01-23 23:23:42 +0800

    Add InteropRequestHandler

---
+++ src/Interop/RequestHandler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Zapheus\Bridge\Psr\Interop;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class RequestHandler implements RequestHandlerInterface
+{
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+    }
+}
