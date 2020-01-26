feb8ce869fca6efe5bbc5eb3eb66ec6b8892614b
Rougin Gutib <rougingutib@gmail.com>
2020-01-26 23:24:57 +0800

    Add FoodController

---
+++ tests/Fixture/FoodController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Zapheus\Bridge\Psr\Fixture;
+
+/**
+ * Food Controller
+ *
+ * @package Zapheus
+ * @author  Rougin Gutib <rougingutib@gmail.com>
+ */
+class FoodController
+{
+    /**
+     * Eats the food.
+     *
+     * @return string
+     */
+    public function eat()
+    {
+        return 'Eating...';
+    }
+}
